8b78406c6283c14ad0ba596580156aa899902e13
sstock2005 <144393153+sstock2005@users.noreply.github.com>
2026-03-02 08:46:58 -0600

    Added routing to the angular application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [Home, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
